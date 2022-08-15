708a956420a82778df705d08f779346e0a50ffb5
unknown <maxivazquez232@gmail.com>
2022-08-15 01:16:00 -0300

    test

--- bot.js
+++ bot.js
@@ -25,11 +25,13 @@ client.on("messageCreate", async msg => {
         if (msg.content.startsWith('/play')) {
             // Only try to join the sender's voice channel if they are in one themselves
             if (msg.member.voice.channel) {
+                console.log("play")
                 const connection = await msg.member.voice.channel.join();
                 const args = msg.content.split(' ').slice(1)
                 const ytdl = require('ytdl-core')
                 connection.play(ytdl(args.join(" ")))
             } else {
+                console.log("no")
                 msg.reply('You need to join a voice channel first!');
             }
         }
